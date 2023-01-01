f5078d4b2d5f0e5f90b982f0ca48d22eb3d6b078
Olena <7890572@ukr.net>
2023-01-01 20:41:40 +0100

    Added code

---
+++ math.js
@@ -0,0 +1,12 @@
+const array = [];
+
+Array.isArray(array);
+console.log(array);
+
+function randomElement(array) {
+    return Math.random() * array;
+  }
+    
+  console.log(randomElement(Math.random()));
+
+  
